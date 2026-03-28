abcb970d6535c413b18d92fc12c94d32dc9b6965
Arseniy <Lychagin2103@mail.ru>
2026-03-28 13:20:22 +0300

    first commit

---
+++ lab_02/base_list.h
@@ -0,0 +1,9 @@
+#ifndef BASE_LIST_H__
+
+#define BASELIST_H__
+
+class BaseList
+{
+};
+
+#endif
---
+++ lab_02/list.h
@@ -0,0 +1,11 @@
+#ifndef LIST_H__
+
+#define LIST_H__
+
+#include "base_list.h"
+
+class List final: public BaseList
+{
+};
+
+#endif
